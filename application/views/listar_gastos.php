806e08085236ad9cbbc3d03ead1f142aa7b2ca5d
Jwnns <jenniferbeatrizmn@gmail.com>
2021-02-23 17:07:21 -0300

    Criação dos controllers Gastos, Categorias e suas views

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Document </title>
+</head>
+<body>
+    
+    <table>
+        <tr><td>Data</td><td>Descrição</td><td>Valor</td></tr>
+        <tr><td>20/02/2021</td><td>Transporte</td><td>R$ 4,00</td></tr>
+        <tr><td>21/02/2021</td><td>Lanche</td><td>R$ 3,50</td></tr>
+        <tr><td>22/02/2021</td><td>Remédio para dor de cabeça</td><td>R$ 5,00</td></tr>
+    </table>
+
+</body>
+</html>
